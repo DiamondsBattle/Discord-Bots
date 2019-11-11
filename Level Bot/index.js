dba4b285ac0c8e673418211ca4cddec64b122838
DiamondsBattle <54222931+DiamondsBattle@users.noreply.github.com>
2019-11-11 13:41:51 +0100

    Update index.js

--- Level Bot/index.js
+++ Level Bot/index.js
@@ -1,21 +1,77 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const Commando = require('discord.js-commando')
+const discord = require('discord.js');
+const random = require('random');
+const fs = require('fs');
+const jsonfile = require('jsonfile');
+
+
+const bot = new Commando.Client();
+
+var stats = {};
+if (fs.existsSync('stats.json')) {
+    stats = jsonfile.readFileSync('stats.json');
+}
 
 bot.on('message', (message) => {
+    if (message.author.id == bot.user.id)
+        return;
 
-    if(message.content == '//help') {
-        message.channel.send('Pas encore disponible');
+    if (message.guild.id in stats === false) {
+        stats[message.guild.id] = {};
+    }
 
-    }else if(message.content == '//repo'); {
-        message.channel.send('Vous pouvez trouvez la dernière version des fichiers de Blade à : https://github.com/diamondsbattle/blade-browser');
+    const guildStats = stats[message.guild.id];
+    if (message.author.id in guildStats === false) {
+        guildStats[message.author.id] = {
+            xp: 0,
+            level: 0,
+            last_message: 0,
+            last_boost: 0,
+            last_random_xp: 0
+        };
+    }
 
-    }//else if(message.content == '//cookie'); {
-//        message.channel.sendMessage(':cookie:');
+    const userStats = guildStats[message.author.id];
+    if (Date.now() - userStats.last_message > 15000) {
+        userStats.xp += random.int(15, 25);
+        userStats.last_message = Date.now();
+    }
+        
 
-//    }else if(message.content == '//logo'); {
-//        message.channel.sendMessage(':blade_logo:');
-//    }
+    const xpToNextLevel = 5 * Math.pow(userStats.level, 2) + 50 * userStats.level + 100;
+    if (userStats.xp >= xpToNextLevel) {
+        userStats.level ++;
+        userStats.xp = userStats.xp - xpToNextLevel;
+        message.channel.send(message.author + ' a atteint le niveau ' + userStats.level);
+    }
 
+    jsonfile.writeFileSync('stats.json', stats);
+
+    console.log(message.author.username + ' a maintenant ' + userStats.xp + ' d\'xp');
+    console.log(xpToNextLevel + ' XP restant pour passer au prochain niveau! ;)');
+
+    const parts = message.content.split(' ');
+
+    if (parts[0] === '?#boost') {
+        if (Date.now() - userStats.last_boost > 43200000) {
+            message.channel.send(message.author + ' a réclamé son boost quotidien!');
+            userStats.xp += random.int(75, 100);
+            userStats.last_boost = Date.now();
+        }
+    }
+    
+    if (parts[0] === '?#random') {
+        if (Date.now() - userStats.last_random_xp > 7257600000) {
+            let random_xp = random.int(10, 1000);
+            message.channel.send(message.author + ' a gagné ***' + random_xp + ' xp*** grâce à la commande ?#random !');
+            userStats.xp += random_xp;
+            userStats.last_random_xp = Date.now();
+        }
+    }
+    if (parts[0] === '?#stats') {
+        message.channel.send('Vos statistiques sont  : Niveaux : ' + userStats.level + ' ***/*** XP : ' + userStats.xp);
+    }
+    
 });
 
-bot.login('/*Put token here*/');
+bot.login('/*Put bot token here*/');
