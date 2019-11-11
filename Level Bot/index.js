a6dc2b4bc876eeca0c27fff6db7d3bb51dc00f5a
DiamondsBattle <54222931+DiamondsBattle@users.noreply.github.com>
2019-11-11 13:19:42 +0100

    Add files via upload

---
+++ Level Bot/index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+
+    if(message.content == '//help') {
+        message.channel.send('Pas encore disponible');
+
+    }else if(message.content == '//repo'); {
+        message.channel.send('Vous pouvez trouvez la dernière version des fichiers de Blade à : https://github.com/diamondsbattle/blade-browser');
+
+    }//else if(message.content == '//cookie'); {
+//        message.channel.sendMessage(':cookie:');
+
+//    }else if(message.content == '//logo'); {
+//        message.channel.sendMessage(':blade_logo:');
+//    }
+
+});
+
+bot.login('NjIzOTY2MzA4OTc4OTE3Mzc2.XY0QVw.Fh3umOcpC-6tNPGwS3CtLQkmfno');
